cffef14e3577202be837c310a5bb69227a5a6c74
justinWGU <Justin_ortiz123@yahoo.com>
2025-01-06 15:10:20 -0800

    Adds str method to Lesson model

--- lingo_learn/core/models.py
+++ lingo_learn/core/models.py
@@ -8,3 +8,7 @@ class Lesson(models.Model):
     difficulty = models.CharField(max_length=20)
     subject = models.CharField(max_length=50)
     contents = models.TextField(max_length=1000)
+
+    def __str__(self):
+        # determines how the object is printed in the django shell & admin
+        return self.subject
