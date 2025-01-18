bd6ca750d154eb19e20b0dca005fd412b5679c97
justinWGU <Justin_ortiz123@yahoo.com>
2025-01-17 19:22:21 -0800

    Adds Test, Question,and Choice models to hold initial language proficieny level test

--- lingo_learn/core/models.py
+++ lingo_learn/core/models.py
@@ -12,3 +12,28 @@ class Lesson(models.Model):
     def __str__(self):
         # determines how the object is printed in the django shell & admin
         return self.subject
+
+
+class Test(models.Model):
+    """Holds the initial test to assess language proficiency level."""
+
+    difficulty = models.CharField(max_length=50) # TODO: Change this to choices
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    """Holds each test question."""
+
+    test = models.ForeignKey(Test, on_delete=models.CASCADE, related_name="questions")
+    description = models.CharField(max_length=200)
+    correct_answer = models.CharField(max_length=200, null=True)
+
+class Choice(models.Model):
+    """Holds an answer choice for each question."""
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices")
+    description = models.CharField(max_length=100)
+
